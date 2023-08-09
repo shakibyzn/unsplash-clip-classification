53b0f4abc10500a27880f7b3761cec110570c4b9
shakibyzn <shakibyzn@gmail.com>
2023-08-09 16:35:22 +0200

    added app.py using flask and predict pipeline

--- src/clipClassifier/utils/common.py
+++ src/clipClassifier/utils/common.py
@@ -4,6 +4,7 @@ from box.exceptions import BoxValueError
 from pathlib import Path
 import yaml
 from clipClassifier import logger
+import base64
 
 
 @ensure_annotations
@@ -20,3 +21,10 @@ def read_yaml(path: Path):
         content = yaml.safe_load(yaml_file)
         logger.info(f"yaml file: {path} loaded successfully")
         return content
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
